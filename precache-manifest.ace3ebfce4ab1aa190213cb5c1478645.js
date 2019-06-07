f3f248d1c7dda6f2e6dcd3a9261b2246f193a615
Vitor Rodrigues Costa <vitor_costa@virgo.atlantico.net.br>
2019-06-07 20:21:57 -0300

    Updates

---
+++ precache-manifest.ace3ebfce4ab1aa190213cb5c1478645.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47386c86d3b45fd4dee00783195fed6",
+    "url": "/daichi_lessons/index.html"
+  },
+  {
+    "revision": "3920ed2b519efb69638b",
+    "url": "/daichi_lessons/static/css/main.d05d4541.chunk.css"
+  },
+  {
+    "revision": "2d6a4bf4d811b0453f83",
+    "url": "/daichi_lessons/static/js/2.cc714a78.chunk.js"
+  },
+  {
+    "revision": "3920ed2b519efb69638b",
+    "url": "/daichi_lessons/static/js/main.f83dbdbb.chunk.js"
+  },
+  {
+    "revision": "8f608bbed2a5e1a24201",
+    "url": "/daichi_lessons/static/js/runtime~main.a03bb54f.js"
+  },
+  {
+    "revision": "4ecec60ac27b5db62034a6cccdd62973",
+    "url": "/daichi_lessons/static/media/background.4ecec60a.jpg"
+  },
+  {
+    "revision": "e9bef895b8b80f369bd0897343d7d34a",
+    "url": "/daichi_lessons/static/media/radial-stripes.e9bef895.svg"
+  }
+]);
